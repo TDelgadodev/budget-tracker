f9e7b11cf5c028f41e52095d14528e05d3d13e65
Thiago <thiagodelgado.dev@gmail.com>
2024-05-08 15:57:57 -0300

    navbar mobile with theme switcher

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,25 +1,67 @@
 "use client";
-
-import React from "react";
-import Logo from "@/components/logo";
-import Link from "next/link";
+import Logo, { LogoMobile } from "@/components/Logo";
+import { ThemeSwitcherBtn } from "@/components/ThemeSwitcherBtn";
+import { Button, buttonVariants } from "@/components/ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { cn } from "@/lib/utils";
-import { buttonVariants } from "./ui/button";
+import { UserButton } from "@clerk/nextjs";
+import { Menu } from "lucide-react";
+import Link from "next/link";
 import { usePathname } from "next/navigation";
+import React, { useState } from "react";
 
-const Navbar = () => {
+function Navbar() {
   return (
     <>
       <DesktopNavbar />
+      <MobileNavbar />
     </>
   );
-};
+}
 
 const items = [
   { label: "Dashboard", link: "/" },
   { label: "Transactions", link: "/transactions" },
   { label: "Manage", link: "/manage" },
 ];
+
+function MobileNavbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="block border-separate bg-background md:hidden">
+      <nav className="container flex items-center justify-between px-8">
+        <Sheet open={isOpen} onOpenChange={setIsOpen}>
+          <SheetTrigger asChild>
+            <Button variant={"ghost"} size={"icon"}>
+              <Menu />
+            </Button>
+          </SheetTrigger>
+          <SheetContent className="w-[400px] sm:w-[540px]" side="left">
+            <Logo />
+            <div className="flex flex-col gap-1 pt-4">
+              {items.map((item) => (
+                <NavbarItem
+                  key={item.label}
+                  link={item.link}
+                  label={item.label}
+                  clickCallback={() => setIsOpen((prev) => !prev)}
+                />
+              ))}
+            </div>
+          </SheetContent>
+        </Sheet>
+        <div className="flex h-[80px] min-h-[60px] items-center gap-x-4">
+          <LogoMobile />
+        </div>
+        <div className="flex items-center gap-2">
+          <ThemeSwitcherBtn />
+          <UserButton afterSignOutUrl="/sign-in" />
+        </div>
+      </nav>
+    </div>
+  );
+}
 
 function DesktopNavbar() {
   return (
@@ -36,6 +78,10 @@ function DesktopNavbar() {
               />
             ))}
           </div>
+        </div>
+        <div className="flex items-center gap-2">
+          <ThemeSwitcherBtn />
+          <UserButton afterSignOutUrl="/sign-in" />
         </div>
       </nav>
     </div>
