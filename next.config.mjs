c879aaf27d8fe701b1f26368c1938244c3105b9f
Thiago <thiagodelgado.dev@gmail.com>
2024-05-26 12:45:19 -0300

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
-const path = require('path');
+import path from 'path';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config) => {
-    config.resolve.alias['@'] = path.resolve(__dirname);
+    config.resolve.alias['@'] = path.resolve('./');
     return config;
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
